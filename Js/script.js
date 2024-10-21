eccee179553e13fe1b2246a738b733da2471c618
Callebe Miranda Cavalcante <callebectbj@gmail.com>
2024-10-21 17:28:29 -0300

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,32 @@
+//sidebar
+
+const toggleBtn = document.getElementById('toggleSidebar');
+
+toggleBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-expanded');
+});
+
+//Slides
+
+let slideIndex = [1,1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1,3);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
